05673ddca762a5e3b2963d1350c0c6e054726060
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-04-29 17:18:59 +0200

    Chat-Online part 1

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+// import { Avatar } from '@mui/material'
+// import React from 'react'
+
+// const Cat = () => {
+//   return (
+//     <Avatar src="https://s3-alpha-sig.figma.com/img/c11e/b8d6/478d616ae22ee9a8c7106c51d82de4d1?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GtSecTm~-weZSC7lDdO5sfbRnAkzVqVup5y5LRNZw58ew4nkwERxKMZSOI2qA0g-59WOf-M0hhBBN60F32090wHpBbEV1l~vFY99IJiTRoNBtnf3R~2n7DWVfihn48Ai4W2w2UAHjqa-o7-FDw0COrPJuA2TKWCjIe~U3CILYveAdumhAAt-SY6p3jsckB32ghvcsWfaeJJhzQ9BoVrW1QIveKy5-NaqBcI44LXLiu92FosUwOdPGrpDJSybQXXaGxxvLJPtF16cgSp-8Ta79udKdkgZgKX275HuIj8rpDfVPKh2XLYxnICwiI124OQGCvereTHWeRcA4n4YZQPBrw__" />
+//   )
+// }
+
+// export default Cat
---
+++ src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Chats from "./Chats";
+import { Box, Stack, useTheme } from "@mui/material";
+import Conversation from "../../components/Conversation";
+
+const GeneralApp = () => {
+  const theme = useTheme()
+
+  return (
+    <Stack direction="row" sx={{width: "100%"}}>
+      {/* Chats */}
+      <Chats />
+
+      {/* Conversation */}
+      <Box sx={{height: "100%", width: "calc(100vw - 420px)", backgroundColor: theme.palette.mode === 'light' ? "#fff" : theme.palette.background.default}}>
+        {/* Conversation component */}
+        <Conversation />
+      </Box>
+    </Stack>
+  );
+};
+
+export default GeneralApp;
